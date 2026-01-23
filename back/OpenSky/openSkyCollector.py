664cad69e85914b4c74a2a3792fbeaf9a3519625
Laurent Ngo <laurent@ngo.com>
2026-01-23 16:37:50 +0100

    updated quality gates

--- back/OpenSky/openSkyCollector.py
+++ back/OpenSky/openSkyCollector.py
@@ -9,7 +9,7 @@ class FirefleetCollector:
 
     def get_positions(self, icao_list):
         # Construct the query parameters
-        params = [('icao24', icao) for icao in icao_list]
+        params = [('callsign', icao) for icao in icao_list]
         
         # New Header Format for OAuth2 / JWT
         headers = {
@@ -34,6 +34,37 @@ class FirefleetCollector:
             print(f"Auth Error: {e.response.status_code} - Check if the token has expired.")
             return []
 
+    def get_by_callsigns(self, target_callsigns):
+            """Fetches all states and filters by a list of callsigns."""
+            headers = {"Authorization": f"Bearer {self.token}"}
+            
+            try:
+                # We fetch all (or use a bounding box for FinOps efficiency)
+                response = requests.get(self.url, headers=headers, timeout=15)
+                response.raise_for_status()
+                data = response.json()
+                
+                states = data.get('states', [])
+                
+                # Filtering in Python
+                # Callsigns in OpenSky are 8 chars long, often padded with spaces
+                matched_fleet = []
+                for s in states:
+                    raw_callsign = s[1].strip()
+                    if raw_callsign in target_callsigns:
+                        matched_fleet.append({
+                            "icao24": s[0],
+                            "callsign": raw_callsign,
+                            "lat": s[6],
+                            "lon": s[5],
+                            "alt": s[7]
+                        })
+                return matched_fleet
+
+            except Exception as e:
+                print(f"Error: {e}")
+                return []
+            
 # --- Local Test ---
 if __name__ == "__main__":
     # In GitLab, we will use os.getenv('OPENSKY_CLIENT_SECRET')
@@ -41,10 +72,10 @@ if __name__ == "__main__":
 
     print(f"DEBUG: Token found: {'Yes' if TOKEN else 'No'}")
     
-    ICAO_TARGETS = ["3b760a", "3b760b"] # Example water bombers
+    MY_CALLSIGNS = ["FRBAQ", "PHBVK", "N937MA", "CAT724"] # Example water bombers
 
     collector = FirefleetCollector(TOKEN)
-    fleet_status = collector.get_positions(ICAO_TARGETS)
+    fleet_status = collector.get_by_callsigns(MY_CALLSIGNS)(MY_CALLSIGNS)
     
     for plane in fleet_status:
-        print(f"Unit {plane['icao24']} | Alt: {plane['alt']}m | Spd: {plane['velocity']}m/s")
+        print(f"Unit {plane}")
--- envs.sh
+++ envs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Check if both arguments are provided
+# Check if argument is provided
 if [ -z "$1" ] ; then
     echo "Usage: $0 <client_secret>"
     exit 1
 fi
 
-back/OpenSky/OAuth2ClientCredential.sh laurent.ngo-api-client $1
+source back/OpenSky/OAuth2ClientCredential.sh laurent.ngo-api-client $1
