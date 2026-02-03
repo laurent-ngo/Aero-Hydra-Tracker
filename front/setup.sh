bf925f516dcbbc8e07cb16016db341161283430f
Laurent Ngo <laurent@ngo.com>
2026-02-03 14:24:52 +0100

    new front end setup

--- front/setup.sh
+++ front/setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Create the project
 npm create vite@latest aero-hydra-ui -- --template react
+
+# Enter the directory
+cd aero-hydra-ui
+
+# Install dependencies
+npm install
+
+# Install the "Adventurer" essentials (Tailwind and Leaflet)
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+
+npm install react-leaflet leaflet lucide-react
