856192cc3f14d6aec2ac83998582c222d8fd012c
Laurent Ngo <laurent@ngo.com>
2026-02-18 10:43:04 +0100

    optimisation of API credit usage

--- back/OpenSky/dataCollector.sh
+++ back/OpenSky/dataCollector.sh
@@ -11,5 +11,5 @@ cd $PROJECT_HOME/back/OpenSky/src
 
 # 4. Run the script using the full path to the virtualenv python
 . $PROJECT_HOME/back/OpenSky/back_end.sh token 2>&1
-#python dataCollector.py $ACTIVE_FLAG 2>&1
+python dataCollector.py $ACTIVE_FLAG 2>&1
 python dataProcessor.py $ACTIVE_FLAG 2>&1
