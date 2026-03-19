0e5daa9b8e1180c3ce73b414b2b6b25553e3ca11
Laurent Ngo <laurent@ngo.com>
2026-03-19 11:43:59 +0100

    fixing AGL

--- back/OpenSky/src/dataProcessor.py
+++ back/OpenSky/src/dataProcessor.py
@@ -212,7 +212,7 @@ def proximity_check( point, airfields, radius_km, alt_threshold_ft):
             return af
     return None
 
-def label_flight_phases(threshold_ft=750, water_threshold_ft=2, airfield_radius=8.0, airfield_alt_threshold=1500):
+def label_flight_phases(threshold_ft=750, water_threshold_ft=10, airfield_radius=8.0, airfield_alt_threshold=1500):
     # 1. Load all airfields into memory for fast lookup
     airfields = db.query(migrate.Airfield).all()
     
